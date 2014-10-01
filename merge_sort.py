8dab76b02532ef175abaad9912c00bd7b8576f42
jeffgodwyll <jeffgodwyll@gmail.com>
2014-10-01 23:23:58 +0000

    Merge sort

--- merge_sort.py
+++ merge_sort.py
@@ -11,15 +11,17 @@ def merge(left, right):
     sorted_list += left[i:]
     sorted_list += right[j:]
     return sorted_list
-    
+
+
 def merge_sort(my_list):
     if len(my_list) <= 1:
         return my_list
-    
+
     mid = len(my_list) / 2
     left = merge_sort(my_list[:mid])
     right = merge_sort(my_list[mid:])
     return merge(left, right)
-    
+
+
 l = [8, 100, 89, 1, 99, 20, 15, 46, 3, 2]
 print merge_sort(l)
